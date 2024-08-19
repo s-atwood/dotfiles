6340b36f183cce0cf69453e3c84b40cbf85c5e0a
Shawn Atwood <sdatwood4@gmail.com>
2024-08-19 01:52:16 -0400

    changes

---
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    config = function() -- This line should be indented to be inside the plugin specification
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.black,
+          null_ls.builtins.formatting.isort,
+          -- null_ls.builtins.diagnostics.pylint,
+          -- require("none-ls.diagnostics.eslint"),
+        },
+      })
+
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
+}
